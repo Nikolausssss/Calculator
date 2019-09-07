2c5d58f80bd9af115782716df41d62d1f1394a88
Nikolausssss <51060686+Nikolausssss@users.noreply.github.com>
2019-09-07 15:51:47 +0500

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,68 +1,13 @@
 "use strict";
 
-
-//let dial = document.querySelector(".dial");
-
-//dial.onclick = calculator.eventwrite;
-
-
-//class Calculator {
-    // let mov = null;
-    // let firstNum = '';
-    // let secondNum = '';
- 
-
-    // //constructor( inputbar, dialbar, movbar ) {
-    //   //  inputbar = inputbar;
-    //     //dialbar = dialbar;
-    //     //movbar = movbar;
-
-    //     //dialbar.onclick = eventwrite;
-    //     //movbar.onclick = eventmov;
-    // //};
-
-
-    // let num ='';
-    
-    // let eventwrite = function(event) {
-       
-    //     if ( event.target.tagName != "DIV" ) {
-    //         firstNum += event.target.textContent;
-    //         inputbar.value = firstNum;
-    //     };
-
-    // };
-
-    // let eventmov = function(event) {
-    //     if ( event.target.tagName != "DIV" ) {
-    //         if (mov != event.target) {
-    //             mov = event.target;
-                
-    //         } else {
-    //             return;
-    //         }
-    //         if ( secondNum == '') {
-    //             secondNum = firstNum;
-    //             firstNum = '';
-    //         } else {
-    //             secondNum = hui[mov.classList[0]](+secondNum, +firstNum);
-    //             inputbar.value = secondNum;
-    //             firstNum = '';
-    //             mov = null;
-    //         }
-    //     };
-    // };
-
-
-let hui = {
+let operands = {
     equally: function(a, b) {
-        let answer = hui[mov.classList[0]](a, b);
-        return answer;
+        return a;
     },
     backspace: function(a, b) {
         num = "num1";
-        i--;
-        return '';
+        inputbar.value = 0;
+        return 0;
     },
     sum: function(a, b) {
         return a + b;
@@ -85,31 +30,74 @@ let hui = {
 
 };
 
-function Operator() {
-    this.num1 = '';
-    this.move = null;
-    this.num2 = '';
-    this.answer = 0;
-    this.decision = function() {
-        if (this.num2 != '' || this.checkunaroperator()) {
-            this.answer = hui[this.move](+this.num1, +this.num2);
-            inputbar.value = +this.answer;
-        };
+// function Operator() {
+//     this.num1 = '';
+//     this.move = null;
+//     this.num2 = '';
+//     this.answer = 0;
+//     this.decision = function() {
+//         this.answer = operands[this.move](+this.num1, +this.num2);
+//         inputbar.value = +this.answer;
+//     };
+//     this.checkunaroperator = function() {
+//         switch (this.move) {
+//             case "sqr" :
+//             case "sqrt" : 
+//                 return true; 
+//         };
+//         return false;
+//     };
+// };
+
+class Operator {
+    num1 = '';
+    move = null;
+    num2 = '';
+    answer = 0;
+    constructor(num) {
+        this.num1 = num;
     };
-    this.checkunaroperator = function() {
+    decision = function() {
+        this.answer = operands[this.move](+this.num1, +this.num2);
+        inputbar.value = +this.answer;
+    };
+    checkunaroperator = function() {
         switch (this.move) {
             case "sqr" :
-            case "sqrt" :
-            case "backspace": 
+            case "sqrt" : 
                 return true; 
-            default: return false;
         };
+        return false;
     };
-};
+}
+
+let addtohistory = function(operation) {
+    let div = document.createElement('div');
+    div.className = "operation";
+
+    let movechar;
+    switch (operation.move) {
+        case "sum": movechar = "+"; break;
+        case "dif": movechar = "-"; break;
+        case "div": movechar = "/"; break;
+        case "mult": movechar = "x"; break;
+        case "sqr": movechar = "<sup>2</sup>"; break;
+        case "sqrt": movechar = "&Sqrt;"; break;
+    }
+    if (operation.move == "sqr") {
+    div.innerHTML = `<p class="operations-value"> ${+operation.num1}${movechar} = ${operation.answer} </p>`;
+    } else if (operation.move == "sqrt") {
+    div.innerHTML = `<p class="operations-value"> ${movechar}${+operation.num1} = ${operation.answer} </p>`;
+    } else {
+        div.innerHTML = `<p class="operations-value"> ${+operation.num1} ${movechar} ${+operation.num2} = ${operation.answer} </p>`;
+    }
+
+    historybar.append(div);
+}
 
 let history = [];
 let i = 0;
-history[i] = new Operator();
+history[i] = new Operator('');
 let num = "num1";
 
 let eventwrite = function(event) {
@@ -122,17 +110,24 @@ let eventwrite = function(event) {
 let eventmov = function(event) {
     if ( event.target.tagName != "DIV" ) {
         num = "num2";
-        // if (event.target.classList[0] == "backspace") {
-            
-        // }
+        let movename = event.target.classList[0];
+
+        if (movename == "backspace") {
+            operands.backspace(0, 0);
+            i++;
+            history[i] = new Operator('');
+        }
+
         if ( history[i].num2 != '' || history[i].checkunaroperator() ) {
             history[i].decision();
+            addtohistory(history[i]);
             i++;
-            history[i] = new Operator();
-            history[i].num1 = inputbar.value || '';
+            history[i] = new Operator(inputbar.value || '' || '');
+            //history[i].num1 = ;
         }
-        if ( event.target.classList[0] != "equally" ) {
-            history[i].move = event.target.classList[0];       
+
+        if ( movename != "equally" || movename != "backspace") {
+            history[i].move = movename;       
         };
     }
 }
@@ -141,5 +136,6 @@ let eventmov = function(event) {
 let dialbar = document.querySelector( ".dial" );
 let movbar = document.querySelector( ".move" );
 let inputbar = document.querySelector( "input" );
+let historybar = document.querySelector( ".history");
 dialbar.addEventListener( 'click', eventwrite );
 movbar.addEventListener( 'click', eventmov );
